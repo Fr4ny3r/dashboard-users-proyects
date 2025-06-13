ee2a610683828de5f15d159eff5c0660f369ba1b
Fr4ny3r <guevarafranyer09@gmail.com>
2025-06-13 01:27:02 -0400

    #2 react

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,19 @@
+import Layout from "../layouts/Layout.astro"
+import Slist from "./ListSections.astro"
 
+
+{console.log()}
 
 export function Home(){
+
+
+
     return(
-        <section className="bg-blue-500 h-4/6 w-17/18 m-auto">
+        <section
+        className="bg-blue-500/2 backdrop-blur-7 translate-y-2 relative h-17/18 w-17/18 m-auto rounded-l"
+        >
 
         </section>
-    )
+    
+)
 }
